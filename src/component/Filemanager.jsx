3c804a27164dbe297fb9b2c9d3db5e543362cff5
Amjath s <145754786+Amjath-s@users.noreply.github.com>
2024-04-23 20:12:49 +0530

    Add files via upload

--- src/component/Filemanager.jsx
+++ src/component/Filemanager.jsx
@@ -6,74 +6,77 @@ import browse from './image/browse.svg'
 import { useState } from "react";
 
 import { Uploadpopup } from "./Uploadpopup";
-import { Scanpopup } from "./Scanpopup";
+// import { Scanpopup } from "./Scanpopup";
 import { Browsepop } from "./Browsepop";
+import { Scanpopup } from "./Scanpopup";
 
 
 
-export function Filemanager()
-{
-    const [popstate,Setpopupsate]=useState(false)
-    const [scanpop,Setscanpop]=useState(false)
-    const[browsepop,Setbrowsepop]=useState(false)
-   
-    const fileaction=()=>{
+export function Filemanager() {
+    const [popstate, Setpopupsate] = useState(false)
+    const [scanpop, Setscanpop] = useState(false)
+    const [browsepop, Setbrowsepop] = useState(false)
+
+    const fileaction = () => {
         Setpopupsate(true)
-      
-    
-   
-        
+
+
+
+
     }
-    const hanldepopclose=()=>{
+    const hanldepopclose = () => {
         Setpopupsate(false)
         Setscanpop(false)
- 
+        Setbrowsepop(false)
+
+        console.log("closed popup")
+
 
     }
-    const filebrowse=()=>{
+    const filebrowse = () => {
         Setbrowsepop(true)
-        
+
     }
-    const filescan=()=>{
+    const filescan = () => {
         Setscanpop(true);
-
-    
+        console.log("Hi");
+        console.log(scanpop)
     }
 
-   
+console.log(scanpop)
 
-    return(
+    return (
         <>
-        <div className="filemanager">
-            <div className="inner-filemanager"> 
-            <div className="container"onClick={fileaction}>
-            <img src={svgimage} alt="svg" />
-            UPLOAD
-             </div>
-            </div>
-            <div className="inner-filemanager">
-            <div className="container" onClick={filescan}>
-           <img src={scan} alt="" />
-            SCAN
-             </div>
-            </div>
-            <div className="inner-filemanager">
-            <div className="container" onClick={filebrowse}>
-            <img src={browse} alt="browse" />
-            BROWSE
-             </div>
-            </div>
-            {popstate&&(<Uploadpopup onclose={hanldepopclose}/>)}
-            {scanpop && (<Scanpopup onclose={hanldepopclose}/>)}
-            {browsepop &&(<Browsepop/>)}
+            <div className="filemanager">
+                <div className="inner-filemanager">
+                    <div className="container" onClick={fileaction}>
+                        <img src={svgimage} alt="svg" />
+                        UPLOAD
+                    </div>
+                </div>
+                <div className="inner-filemanager">
+                    <div className="container" onClick={() => { filescan() }}>
+                        <img src={scan} alt="" />
+                        SCAN
+                    </div>
+                </div>
+                <div className="inner-filemanager">
+                    <div className="container" onClick={filebrowse}>
+                        <img src={browse} alt="browse" />
+                        BROWSE
+                    </div>
+                </div>
+                {popstate && (<Uploadpopup onclose={hanldepopclose} />)}
+                {scanpop && (<Scanpopup onclose={hanldepopclose} />)}
+                {browsepop && (<Browsepop  onclose={hanldepopclose}/>)}
 
-             </div>
-        
-        
+            </div>
+
+
         </>
-        
-    
+
+
 
     )
-    
+
 }
